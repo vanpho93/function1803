764aa299bc23add0e84fa7b626782a5ab2411291
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-25 09:26:08 +0700

    Line

--- h.js
+++ h.js
@@ -10,8 +10,32 @@ class Point {
     }
 }
 
-const p1 = new Point(1, 1);
-const p2 = new Point(Math.sqrt(2), Math.sqrt(2));
+class Line {
+    constructor(pointA, pointB) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
 
-console.log(p1.getDistance());
-console.log(p2.getDistance());
+    getLength() {
+        const dx = this.pointA.x - this.pointB.x;
+        const dy = this.pointA.y - this.pointB.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+class Triangle {
+    constructor(pointA, pointB, pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
+    }
+
+    getPerimeter() {}
+}
+
+const p1 = new Point(2, 2);
+const p2 = new Point(1, 1);
+
+const l1 = new Line(p1, p2);
+
+console.log(l1.getLength());
