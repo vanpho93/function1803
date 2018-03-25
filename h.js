740d0a47d934671a6c16c0061fbbabe4a9f635e5
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-25 09:37:02 +0700

    getPerimeter

--- h.js
+++ h.js
@@ -30,12 +30,17 @@ class Triangle {
         this.pointC = pointC;
     }
 
-    getPerimeter() {}
+    getPerimeter() {
+        const l1 = new Line(this.pointA, this.pointB);
+        const l2 = new Line(this.pointA, this.pointC);
+        const l3 = new Line(this.pointB, this.pointC);
+        return l1.getLength() + l2.getLength() + l3.getLength();
+    }
 }
 
-const p1 = new Point(2, 2);
-const p2 = new Point(1, 1);
-
+const p1 = new Point(0, 1);
+const p2 = new Point(1, 0);
+const p3 = new Point(0, 0)
 const l1 = new Line(p1, p2);
-
-console.log(l1.getLength());
+const tr1 = new Triangle(p1, p2, p3);
+console.log(tr1.getPerimeter());
